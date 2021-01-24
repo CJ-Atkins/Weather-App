95f2dbe0281e60c50d321762243e08e5857e79ef
Connor Atkins <connoratkins@hotmail.co.uk>
2021-01-24 11:38:50 +0000

    updated UI

--- js/app.js
+++ js/app.js
@@ -1,33 +1,36 @@
 const weather = new Weather();
 const ui = new UI();
 
-const searchBtn = document.getElementById('search-button'),
-  locationInput = document.getElementById('location-input'),
-  resultsContainer = document.getElementById('results-container');
+const searchBtn = document.getElementById("search-button"),
+  locationInput = document.getElementById("location-input"),
+  locationContainer = document.querySelector(".location"),
+  resultsContainer = document.getElementById("results-container");
 
 pageLoad();
 
 function pageLoad() {
-  let locationLocalStorage = localStorage.getItem('location');
-  if (localStorage.getItem('location') === null) {
-    console.log('no local storage');
+  let locationLocalStorage = localStorage.getItem("location");
+  if (localStorage.getItem("location") === null) {
+    console.log("no local storage");
   } else {
     weather.getWeather(locationLocalStorage).then((data) => {
-      resultsContainer.style.display = 'block';
+      locationContainer.style.display = "block";
+      resultsContainer.style.display = "flex";
       ui.updateUI(data);
     });
   }
 }
 
 // EVENT LISTENER
-searchBtn.addEventListener('click', function (e) {
-  if (locationInput.value !== '') {
-    resultsContainer.style.display = 'block';
+searchBtn.addEventListener("click", function (e) {
+  if (locationInput.value !== "") {
+    locationContainer.style.display = "block";
+    resultsContainer.style.display = "flex";
     weather.getWeather(locationInput.value).then((data) => {
       ui.updateUI(data);
     });
   }
 
-  localStorage.setItem('location', locationInput.value);
+  localStorage.setItem("location", locationInput.value);
   e.preventDefault();
 });
--- js/ui.js
+++ js/ui.js
@@ -1,37 +1,22 @@
 class UI {
   constructor() {
-    (this.location_result = document.getElementById('location')),
+    (this.location_result = document.getElementById("location")),
       // DAY ONE
-      (this.day_one_icon = document.getElementById('day-one-icon')),
-      (this.day_one_condition = document.getElementById('day-one-condition')),
+      (this.day_one_icon = document.getElementById("day-one-icon")),
+      (this.day_one_condition = document.getElementById("day-one-condition")),
       (this.day_one_temperature = document.getElementById(
-        'day-one-temperature'
+        "day-one-temperature"
       )),
-      (this.day_one_wind = document.getElementById('day-one-wind')),
-      (this.day_one_rise = document.getElementById('day-one-rise')),
-      (this.day_one_set = document.getElementById('day-one-set')),
+      (this.day_one_rise = document.getElementById("day-one-rise")),
+      (this.day_one_set = document.getElementById("day-one-set")),
       // DAY TWO
-      (this.day_two_icon = document.getElementById('day-two-icon')),
-      (this.day_two_condition = document.getElementById('day-two-condition')),
+      (this.day_two_icon = document.getElementById("day-two-icon")),
+      (this.day_two_condition = document.getElementById("day-two-condition")),
       (this.day_two_temperature = document.getElementById(
-        'day-two-temperature'
+        "day-two-temperature"
       )),
-      (this.day_two_wind = document.getElementById('day-two-wind')),
-      (this.day_two_rise = document.getElementById('day-two-rise')),
-      (this.day_two_set = document.getElementById('day-two-set')),
-      (this.day_two_date = document.getElementById('day-two-date')),
-      // DAY THREE
-      (this.day_three_icon = document.getElementById('day-three-icon')),
-      (this.day_three_condition = document.getElementById(
-        'day-three-condition'
-      )),
-      (this.day_three_temperature = document.getElementById(
-        'day-three-temperature'
-      )),
-      (this.day_three_wind = document.getElementById('day-three-wind')),
-      (this.day_three_rise = document.getElementById('day-three-rise')),
-      (this.day_three_set = document.getElementById('day-three-set')),
-      (this.day_three_date = document.getElementById('day-three-date'));
+      (this.day_two_rise = document.getElementById("day-two-rise")),
+      (this.day_two_set = document.getElementById("day-two-set"));
   }
 
   updateUI(data) {
@@ -40,24 +25,14 @@ class UI {
     this.day_one_condition.innerHTML = `${data.current.condition.text}`;
     this.day_one_icon.src = `http:${data.current.condition.icon}`;
     this.day_one_temperature.innerHTML = `${data.current.temp_c}`;
-    this.day_one_wind.innerHTML = `${data.current.wind_mph}mph`;
+
     this.day_one_rise.innerHTML = `${data.forecast.forecastday[0].astro.sunrise}`;
     this.day_one_set.innerHTML = `${data.forecast.forecastday[0].astro.sunset}`;
     // DAY TWO
-    this.day_two_date.innerHTML = `${data.forecast.forecastday[1].date}`;
     this.day_two_condition.innerHTML = `${data.forecast.forecastday[1].day.condition.text}`;
     this.day_two_icon.src = `http:${data.forecast.forecastday[1].day.condition.icon}`;
     this.day_two_temperature.innerHTML = `${data.forecast.forecastday[1].day.avgtemp_c}`;
-    this.day_two_wind.innerHTML = `${data.forecast.forecastday[1].day.maxwind_mph}mph`;
     this.day_two_rise.innerHTML = `${data.forecast.forecastday[1].astro.sunrise}`;
     this.day_two_set.innerHTML = `${data.forecast.forecastday[1].astro.sunset}`;
-    // DAY THREE
-    this.day_three_date.innerHTML = `${data.forecast.forecastday[2].date}`;
-    this.day_three_condition.innerHTML = `${data.forecast.forecastday[2].day.condition.text}`;
-    this.day_three_icon.src = `http:${data.forecast.forecastday[2].day.condition.icon}`;
-    this.day_three_temperature.innerHTML = `${data.forecast.forecastday[2].day.avgtemp_c}`;
-    this.day_three_wind.innerHTML = `${data.forecast.forecastday[2].day.maxwind_mph}mph`;
-    this.day_three_rise.innerHTML = `${data.forecast.forecastday[2].astro.sunrise}`;
-    this.day_three_set.innerHTML = `${data.forecast.forecastday[2].astro.sunset}`;
   }
 }
